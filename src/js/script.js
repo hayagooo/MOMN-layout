b2ea5d2be73a16f6ed718c506ad7f0a639ca5f9c
ilhami magfiro <ilhammaghfiro@gmail.com>
2020-11-07 22:02:03 +0700

    navbar scrollTop()

--- src/js/script.js
+++ src/js/script.js
@@ -2,318 +2,49 @@ function redirect(URL) {
     window.location.href = URL
 }
 
-$(document).ready(function () {
-    AOS.init();
-    $('.trackers, .news, .teams').slick({
-        arrows: false,
-        dots: true,
-        speed: 300,
-        autoplay: true,
-        autoplaySpeed: 12000,
-        slidesToShow: 1,
-        slidestoScroll: 1,
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 1,
-                    slidestoScroll: 1,
-                    infinite: true,
-                    dots: true,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidestoScroll: 1,
-                    infinite: true,
-                    dots: true,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToShow: 1,
-                }
-            }
-        ]
-    });
-
-    $('#rekomend-shop').slick({
-        arrows: false,
-        dots: true,
-        speed: 300,
-        autoplay: true,
-        autoplaySpeed: 11000,
-        slidesToShow: 2,
-        slidestoScroll: 1,
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true,
-                }
-            }
-        ]
-    });
-
-
-    $('#rekomend-shop-profile').slick({
-        arrows: false,
-        dots: true,
-        speed: 300,
-        autoplay: true,
-        autoplaySpeed: 11000,
-        slidesToShow: 2,
-        slidestoScroll: 1,
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true,
-                }
-            }
-        ]
-    });
-
-
-    $('#rekomend-shop-detail').slick({
-        arrows: false,
-        dots: true,
-        speed: 300,
-        autoplay: true,
-        autoplaySpeed: 11000,
-        slidesToShow: 3,
-        slidestoScroll: 1,
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidestoScroll: 1,
-                    infinite: true,
-                    dots: true,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidestoScroll: 1,
-                    infinite: true,
-                    dots: true,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToShow: 1,
-                }
-            }
-        ]
-    });
-
-    $('#product-sold').slick({
-        arrows: false,
-        dots: true,
-        speed: 300,
-        autoplay: true,
-        autoplaySpeed: 11000,
-        slidesToShow: 2,
-        centerMode: true,
-        slidestoScroll: 1,
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 1,
-                    slidestoScroll: 1,
-                    infinite: true,
-                    dots: true,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidestoScroll: 1,
-                    infinite: true,
-                    dots: true,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToShow: 1,
-                }
-            }
-        ]
-    });
-
-    $('#shop-banner').slick({
-        arrows: false,
-        dots: true,
-        speed: 300,
-        autoplay: true,
-        autoplaySpeed: 10000,
-        slidesToShow: 1,
-        slidestoScroll: 1,
-        centerMode: true,
-        centerPadding: '10px'
-    });
-
-    $('#shop-preview').slick({
-        arrows: false,
-        slidestoScroll: 1,
-        slidesToShow: 1,
-        dots: false,
-        speed: 300,
-        autoplay: false,
-        fade: true,
-        asNavFor: '#preview-nav'
-    });
-
-    $('#preview-nav').slick({
-        slidesToShow: 2,
-        slidestoScroll: 1,
-        asNavFor: '#shop-preview',
-        dots: true,
-        centerPadding: '10px',
-        centerMode: true,
-        focusOnSelect: true,
-    });
-
-    $('#kupon-slider').slick({
-        slidesToShow: 1,
-        slidestoScroll: 1,
-        dots: true,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 12000,
-        arrows: true,
-        speed: 300
-    })
-    new TypeIt('#typeit_corona', {
-            speed: 250,
-            loop: true,
-        }).type("Dirumah saja", {
-            delay: 300
-        })
-        .pause(3000)
-        .delete()
-        .type("Selalu memakai masker", {
-            delay: 300
-        })
-        .pause(3000)
-        .delete(6)
-        .type("Hand sanitizer", {
-            delay: 300
-        })
-        .pause(3000)
-        .delete()
-        .type("Menggunakan protokol kesehatan", {
-            delay: 300
-        })
-        .pause(5000)
-        .delete()
-        .go();
-
-    new TypeIt('#slogan-donasi', {
-            speed: 250,
-            loop: true,
-        }).type("Menyenangkan..", {
-            delay: 300
-        })
-        .pause(3000)
-        .delete()
-        .type('Membuat hidup berkah..', {
-            delay: 300
-        })
-        .pause(3000)
-        .delete(14)
-        .type('sosialisme semakin meningkat', {
-            delay: 300
-        })
-        .pause(3000)
-        .delete()
-        .go();
-
-    $('#toggler-nav').click(function () {
-        if (!($('navbar-collapse').hasClass('show'))) {
-            $('.burger-nav div:nth-child(1)').toggleClass('burger-cross1');
-            $('.burger-nav div:nth-child(2)').toggleClass('burger-cross2');
-        }
-    });
-
-
-    // Berita script
-
-    $('.card-berita img, .tools').mouseenter(function () {
-        $('.card-berita img').addClass('img_berita-opacity5');
-        $('.tools').removeClass('tools-hide').addClass('tools-show animate__animated animate__bounce');
-    });
-    $('.card-berita img, .tools').mouseleave(function () {
-        $('.card-berita img').removeClass('img_berita-opacity5');
-        $('.tools').removeClass('tools-show animate__animated animate__bounce').addClass('tools-hide');
-    });
-    $('.card-body').mouseenter(function () {
-        $('.card-berita .card-body .card-text').addClass('animate__animated animate__fadeInUp animate__faster');
-    });
-    $('.card-body').mouseleave(function () {
-        $('.card-berita .card-body .card-text').removeClass('animate__animated animate__fadeInUp animate__faster');
-    });
-
-    // End berita script
-
-
-    let token = localStorage.getItem('user');
-    if (token !== null && token !== undefined && token !== "" && token !== "null") {
-        $('#btn-login').addClass('d-none')
-        $('#btn-profil').removeClass('d-none')
+$("body").on('click', '.toggle-password', function () {
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $("#password");
+    if (input.attr("type") === "password") {
+        input.attr("type", "text");
     } else {
-        $('#btn-login').removeClass('d-none')
-        $('#btn-profil').addClass('d-none')
+        input.attr("type", "password");
     }
 
 });
+
+$("#left input").focus(function () {
+    $("#right").attr('class', 'col-md-8 col-12 p-0 mr-0 bg-spring-green')
+    $("#left").attr('class', 'col-md-4 col-12 p-0')
+    $("#btn-masuk").addClass('w-100 my-4')
+    $(".form-login-manual").removeClass('col-md-8').addClass('col-md-10')
+    $(".form-login-manual form").css({
+        'margin-top': '5.2rem'
+    })
+})
+
+$("#right-qr").click(function () {
+    $("#right-qr").removeClass('col-md-8').addClass('col-md-4')
+    $("#left-qr").removeClass('col-md-4').addClass('col-md-8')
+    $(".form-login-qr").removeClass('col-md-10').addClass('col-md-7')
+    $(".login-qr-right").css({
+        'padding-left': '2%',
+        'padding-right': '2%'
+    })
+    $(".form-login-qr form").css({
+        'margin-top': '6rem'
+    })
+    $(".login-qr-right div").addClass('px-3')
+})
+
+// navbar
+$(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+
+    if (wScroll > 10) {
+        $("#navbar-primary").addClass('navbar-gradient px-5 py-3 shadow')
+    } else {
+        $("#navbar-primary").removeClass('navbar-gradient px-5 py-3 shadow')
+    }
+})
+// end navbar
